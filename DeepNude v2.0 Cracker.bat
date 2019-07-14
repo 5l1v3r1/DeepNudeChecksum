eadf7b0ac0cd356f937c9e0f511582532b2027ee
SJWisShit <rijekog@cloud-mail.net>
2019-07-14 19:39:00 -0300

    Fix Crack

--- DeepNude v2.0 Cracker.bat
+++ DeepNude v2.0 Cracker.bat
@@ -5,13 +5,13 @@ del "color.cp36-win_amd64.pyd">NUL
 del "libsqld.cp36-win_amd64.pyd">NUL
 echo def checkcolor():>color.py
 echo     return [255, 240, 255]>>color.py
-echo.>color.py
+echo.>>color.py
 echo def newcolor(a, b):>>color.py
 echo     return 255>>color.py
 echo.>>color.py
 
 echo def add(a, b, c):>libsqld.py
-echo    return a>>libsqld.py
+echo     return a>>libsqld.py
 echo.>>libsqld.py
 
 echo Cracked
